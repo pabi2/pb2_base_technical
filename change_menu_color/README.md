7994605ee5f66d6dd170802d8ff8dc39d7c5dc20
Kitti U. <kittiu@gmail.com>
2016-06-21 23:32:35 +0700

    [ADD] change_menu_color

---
+++ change_menu_color/README.md
@@ -0,0 +1,2 @@
+# odoo-change-menu-color
+Change color of Odoo 8.0 menu
---
+++ change_menu_color/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 1200wd.com
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "Change menu color",
+    'summary': """Change Odoo main menu color""",
+    'description': """
+    Change color of Odoo 8.0 menu
+    """,
+    'author': "1200 Web Development",
+    'website': "http://1200wd.com",
+    'category': 'Custom',
+    'version': '8.0.1.2',
+    'depends': [],
+    'data': [
+         'change_menu_color.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'price': 0.00,
+    'currency': 'EUR',
+    'demo': [],
+}
