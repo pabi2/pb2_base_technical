d847e2f8b25f4b66186f9dc89797b0358143620b
Kitti U <kittiu@gmail.com>
2016-11-14 17:41:02 +0700

    [ADD] account_model_generate_hook

---
+++ account_model_generate_hook/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Account Model Generate Hook',
+    'version': '1.0',
+    'author': 'Ecosoft',
+    'summary': 'Hook for account.model',
+    'description': """
+    """,
+    'category': 'Accounting',
+    'website': 'http://www.ecosoft.co.th',
+    'images': [],
+    'depends': [
+        'account',
+    ],
+    'demo': [],
+    'data': [
+    ],
+    'test': [
+    ],
+    'auto_install': False,
+    'application': False,
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
