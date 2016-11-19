7e712293943af7d081c316a6aafdd8fe7f1f8f96
Kitti U <kittiu@gmail.com>
2016-11-19 16:49:28 +0700

    [ADD] account_invoice_action_move_create_hook

---
+++ account_invoice_action_move_create_hook/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Account Invoice Hooks',
+    'version': '1.0',
+    'category': 'Accounting & Finance',
+    'description': '''
+
+Add hook point in account_invoice.action_move_create()
+
+''',
+    'author': "Ecosoft",
+    'website': 'http://ecosoft.co.th',
+    'depends': ['account'],
+    'data': [],
+    'installable': True,
+}
