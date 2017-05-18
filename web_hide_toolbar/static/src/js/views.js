0009df507fea8247b656ac37bfb33c19fcdbfb18
Kitti U <kittiu@gmail.com>
2017-05-18 21:03:59 +0700

    [ENH] Hide left and top menu from link to another page.

--- web_hide_toolbar/static/src/js/views.js
+++ web_hide_toolbar/static/src/js/views.js
@@ -3,24 +3,29 @@ openerp.web_hide_toolbar = function (instance) {
         start: function () {
             var toolbar = $.deparam.querystring().toolbar;
             if (toolbar === 'hide'){
-                this.hidetoolbar()
+                this.showsheetonly()
             }
             return this._super();
         },
-        hidetoolbar: function(ev) {
-            $("#oe_main_menu_navbar").hide();
+        showsheetonly: function(ev) {
+            $("#oe_main_menu_navbar, .oe_leftbar, .oe_view_manager_header, .oe_chatter").hide();
+            $('.oe_form_sheetbg').css('border-bottom','0px');
+            $('body, .oe_view_manager_body').css('background','url(/web/static/src/img/form_sheetbg.png)');
         },
     });
+    
     instance.web.View.include({
         start: function () {
             var toolbar = $.deparam.querystring().toolbar;
             if (toolbar === 'hide'){
-                this.hidetoolbar()
+                this.showsheetonly()
             }
             return this._super();
         },
-        hidetoolbar: function(ev) {
-            $(".oe_leftbar").hide();
+        showsheetonly: function(ev) {
+        	$("#oe_main_menu_navbar, .oe_leftbar, .oe_view_manager_header, .oe_chatter").hide();
+            $('.oe_form_sheetbg').css('border-bottom','0px');
+            $('body, .oe_view_manager_body').css('background','url(/web/static/src/img/form_sheetbg.png)');
         },
     });
 };
