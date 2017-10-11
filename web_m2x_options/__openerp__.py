743e063f2b249dae4553743650045b4cd76b833b
Kitti U <kittiu@gmail.com>
2017-10-11 16:34:41 +0700

    -

--- web_m2x_options/__openerp__.py
+++ web_m2x_options/__openerp__.py
@@ -2,13 +2,19 @@
 
 {
     "name": 'web_m2x_options',
-    "version": "8.0.0.2",
+    "version": "8.0.0.2.0",
     "depends": [
         'base',
         'web',
     ],
-    'data': ['views/view.xml'],
+    'qweb': [
+        'static/src/xml/base.xml',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'views/view.xml',
+        'views/model_view.xml',
+    ],
     "author": "0k.io,Odoo Community Association (OCA)",
     "installable": True,
-    "active": False,
 }
