d8e36e290ed47bdd7ad4711697b9e380810ae34e
Kitti U <kittiu@gmail.com>
2018-10-18 20:39:28 +0700

    [ADD] account_invoice_anglo_saxon_sale_move_lines

---
+++ account_invoice_anglo_saxon_sale_move_lines/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Account Invoice Hooks',
+    'version': '1.0',
+    'category': 'Accounting & Finance',
+    'description': '''
+
+Add hook point in account_invoice_line._anglo_saxon_sale_move_lines()
+
+''',
+    'author': "Ecosoft",
+    'website': 'http://ecosoft.co.th',
+    'depends': ['account_anglo_saxon', 'account'],
+    'data': [],
+    'installable': True,
+}
