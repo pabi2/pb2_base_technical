820da094ae0d562d767844651f63525707244e64
Jakkrich Changgon <mr.jakkrich@gmial.com>
2019-03-28 16:04:27 +0700

    fix lang update to src

--- web_translate_dialog/models/translation.py
+++ web_translate_dialog/models/translation.py
@@ -32,13 +32,6 @@ class IRTranslation(models.Model):
                                         ('type','=','model'),
                                         ('res_id', '=', res_id)])
             
-            trans_src = trans_search.src.replace(" ", "")
-            trans_value = trans_search.value.replace(" ", "")
-            pattern = re.compile(r"[a-zA-Z]")
-            char_name = re.findall(pattern, data[field_name].replace(" ", ""))
-            char_src = re.findall(pattern, trans_src)
-            char_value = re.findall(pattern, trans_value)
-                    
             if trans_search and lang_code == context['lang']:
                 trans_search.with_context(context).\
                     write({'value': data[field_name]})
@@ -46,9 +39,19 @@ class IRTranslation(models.Model):
                 trans_search.with_context(context).\
                     write({'src': data[field_name], 'source': data[field_name]})
             
+            trans_search = trans.search([('lang','=','th_TH'),
+                                            ('name','=',name),
+                                            ('type','=','model'),
+                                            ('res_id', '=', res_id)])
+            trans_src = trans_search.src.replace(" ", "")
+            trans_value = trans_search.value.replace(" ", "")
+            pattern = re.compile(r"[a-zA-Z]")
+            char_name = re.findall(pattern, data[field_name].replace(" ", ""))
+            char_src = re.findall(pattern, trans_src)
+            char_value = re.findall(pattern, trans_value)
             
             if trans_search and context['lang'] == 'th_TH' and not char_name:
-                trans_search.with_context(context).\
+                trans_search.with_context(lang="th_TH").\
                     write({'value': data[field_name]})
             if trans_search and context['lang'] == 'th_TH' and not char_name and not char_src:
                 trans_search.with_context(lang="en_US").\
